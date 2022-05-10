21b622685bf6f7517d641a4d9e07b0dcb1cf4ffe
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-05-10 21:44:42 +0530

    completed
    created a book api

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+let app = express();
+let PORT = 8000;
+
+app.use(logger);
+// rotes
+
+app.get('/books', (req, res) => {
+  res.send({ route: req.path });
+});
+
+app.get('/libraries', checkPermission('librarian'), (req, res) => {
+    return res.send({ route: req.path, permission: req.permission });
+});
+
+app.get('/authors', checkPermission('author'), (req, res) => {
+    return res.send({ route: req.path, permission: req.permission });
+});
+
+
+// middlewares
+
+function logger(req, res, next) {
+  console.log(req.path);
+  next();
+}
+
+function checkPermission(user) {
+  return (req, res, next) => {
+    if (user === 'librarian' && req.path === '/libraries') {
+      req.permission = true;
+    } else if (user === 'author' && req.path === '/authors') {
+      req.permission = true;
+    } else {
+      console.log('permission denied');
+    }
+    next();
+  };
+}
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`server running on http://localhost:${PORT}`);
+});
